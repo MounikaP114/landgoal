b52319988a978bc1569f5346e2706bfbc62c61cd
Mounika P <mounikapotladurthiofficial@gmail.com>
2024-06-12 15:36:21 +0530

    connected  to mongodb successfully

--- api/Server.js
+++ api/Server.js
@@ -1,6 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-const app= express()
-app.listen(3000,()=>{
-    console.log("App is listening to port")
-})
+dotenv.config();
+
+const app = express();
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("Connected to  mongo db successfully")})
+    .catch((error)=>{
+        console.log("Error while connecting to mongodb,", error)
+    })
+
+app.listen(3000, () => {
+  console.log("App is listening to port");
+});
