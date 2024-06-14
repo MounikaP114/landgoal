f821fbbec57747a1f52271a6bfb578bd0f7e9cc4
Mounika P <mounikapotladurthiofficial@gmail.com>
2024-06-14 20:34:05 +0530

    initial

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
-=======
-import SignUp from "./SignUp";
-import { Link } from "react-router-dom";
-Link;
->>>>>>> 826a9123f8837c265199cf914c605cca35a80df0
 
 export default function SignIn() {
   return (
@@ -15,36 +9,23 @@ export default function SignIn() {
         <h1 className="text-3xl text-center font-semibold mb-6">Sign In</h1>
         <form className="flex flex-col justify-center items-center space-y-6 w-3/4 ">
           <input
-<<<<<<< HEAD
-            className="p-4 bg-slate-100 border rounded-lg w-full  hover:bg-slate-200 focus:outline-none"
-=======
             className="p-4 bg-slate-100 border rounded-lg w-full"
->>>>>>> 826a9123f8837c265199cf914c605cca35a80df0
             type="text"
             placeholder="Email"
           />
           <input
-<<<<<<< HEAD
             className="p-4 bg-slate-100 border rounded-lg w-full hover:bg-slate-200 focus:outline-none"
-=======
-            className="p-4 bg-slate-100 border rounded-lg w-full"
->>>>>>> 826a9123f8837c265199cf914c605cca35a80df0
             type="password"
             placeholder="Password"
           />
-          <button className="bg-slate-700 text-white p-2 rounded-sm w-full hover:opacity-95">
+          <button className="bg-slate-700 text-white p-2 rounded-sm w-full hover:bg-slate-200 focus:outline-none">
             Sign In
           </button>
-          <button className="bg-red-600 text-white p-2 rounded-sm w-full hover:opacity-95">
+          <button className="bg-red-600 text-white p-2 rounded-sm w-full hover:bg-slate-200 focus:outline-none">
             Continue With Google
           </button>
-<<<<<<< HEAD
           <p className="mt-4 flex">Don't Have an account!! </p>
           <Link to={"/signup"}>
-=======
-          <p className="mt-4 flex">Have an account!! </p>
-          <Link to={"/singup"}>
->>>>>>> 826a9123f8837c265199cf914c605cca35a80df0
             <span className="text-blue-600 ml-2">Sign up</span>
           </Link>
         </form>
