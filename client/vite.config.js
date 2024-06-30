d577ead5e3b0ee82e099660af55b68acfbfb4406
Mounika P <mounikapotladurthiofficial@gmail.com>
2024-06-30 18:55:02 +0530

    pre install5

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:3001",
-        changeOrigin: true,
+
         secure: false,
       },
     },
   },
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  }
 });
