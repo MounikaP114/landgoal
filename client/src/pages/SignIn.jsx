8d1fcde1b054e0f039e40017f1c4f3999333b6d6
Mounika P <mounikapotladurthiofficial@gmail.com>
2024-07-01 09:04:00 +0530

    bug fix

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -39,7 +39,7 @@ export default function SignIn() {
         return;
       }
       dispatch(signInSuccess(data));
-      navigate("/home");
+      navigate("/x");
       // Handle successful response here
     } catch (error) {
       dispatch(signInFailure(error.message));
